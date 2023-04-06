72c8944a1540af8c3e11b71ab5a7607de5025f72
Dmitry <dmitry27zh@gmail.com>
2023-04-06 11:26:53 +0500

    Refactor dom module

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,13 +1,40 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ?
+      document.querySelector(selector) :
+      selector
+  }
 
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+
+    this.$el.append(node)
+    return this
+  }
 }
 
-export const $ = function() {
-  return new Dom()
+export const $ = function(selector) {
+  return new Dom(selector)
 }
 
-$.create = function(tagName, className) {
+$.create = function(tagName = 'div', className = '') {
   const $el = document.createElement(tagName)
   $el.className = className
-  return $el
+  return $($el)
 }
