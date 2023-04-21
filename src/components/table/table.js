1929861cb4c246b1f445e0c6d7374f6592cabc32
Dmitry <dmitry27zh@gmail.com>
2023-04-21 12:59:01 +0500

    Start table creat

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,4 +1,5 @@
 import { ExcelComponent } from '@core/excel-component';
+import { createTable } from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table table'
@@ -15,42 +16,6 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return `
-      <div class="table__row">
-        <div class="table__info table__info-empty cell"></div>
-        <div class="table__data">
-          <div class="table__cell table__head cell">A</div>
-          <div class="table__cell table__head cell">B</div>
-          <div class="table__cell table__head cell">C</div>
-          <div class="table__cell table__head cell">D</div>
-          <div class="table__cell table__head cell">E</div>
-          <div class="table__cell table__head cell">J</div>
-          <div class="table__cell table__head cell">K</div>
-        </div>
-      </div>
-      <div class="table__row">
-        <div class="table__info cell">1</div>
-        <div class="table__data">
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-          <div class="table__cell cell" contenteditable>smt1</div>
-        </div>
-      </div>
-      <div class="table__row">
-        <div class="table__info cell">2</div>
-        <div class="table__data">
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-          <div class="table__cell cell" contenteditable>smt2</div>
-        </div>
-      </div>`
+    return createTable()
   }
 }
---
+++ src/components/table/table.template.js
@@ -0,0 +1,62 @@
+const Head = {
+  A: 'A'.codePointAt(),
+  Z: 'Z'.codePointAt(),
+}
+
+const createHead = (head) => {
+  return `
+    <div class="table__cell table__head cell">${head}</div>`
+}
+
+const createHeads = () => {
+  const headsCount = Head.Z - Head.A + 1
+  const heads = Array.from({ length: headsCount }, (_, index) => {
+    return String.fromCodePoint(Head.A + index)
+  })
+
+  return heads.map(createHead).join('')
+}
+
+const createCell = (cell) => {
+  return `
+    <div class="table__cell cell" contenteditable>${cell}</div>`
+}
+
+const createCells = (cells = ['Exc1', 'Exc2', 'Exc3']) => {
+  return cells.map(createCell).join('')
+}
+
+const createRow = (info, data) => {
+  return `
+    <div class="table__row">
+      <div class="table__info cell">${info}</div>
+      <div class="table__data">
+        ${data}
+      </div>
+    </div>`
+}
+
+const createServiceRow = () => {
+  const info = ''
+  const data = createHeads()
+
+  return createRow(info, data)
+}
+
+const createMainRow = (info, cells) => {
+  const data = createCells(cells)
+
+  return createRow(info, data)
+}
+
+const createTable = (rowsCount = 12) => {
+  const serviceRow = createServiceRow()
+  const mainRows = Array.from(
+      { length: rowsCount },
+      (_, index) => createMainRow(index + 1)
+  )
+
+  return [serviceRow, ...mainRows].join('')
+}
+
+export { createTable }
