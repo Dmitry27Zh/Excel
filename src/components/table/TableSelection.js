b14ffa5d4c3f9881bb1c372b702bef3ae6cc22a6
Dmitry <dmitry27zh@gmail.com>
2023-05-21 12:00:46 +0300

    Add select and selectGroup methods

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -1,14 +1,52 @@
 import { Table } from '@/components/table/table';
 
 export class TableSelection {
-  constructor() {
-    this.group = []
+  constructor($dataCells) {
+    this.$dataCells = $dataCells
+    this.selected = null
+    this.selectedGroup = new Set()
+  }
+
+  init() {
+    this.select(this.$dataCells[0])
+    this.addListeners()
   }
 
   select($cell) {
+    this.unselectGroup()
+    this.selectedGroup.add($cell)
     $cell.classList.add(Table.ClassList.SELECTED)
-    this.group.push($cell)
+
+    if ($cell.matches(':focus')) {
+      this.unSelect(this.selected)
+      this.selected = $cell
+    }
   }
 
-  selectGroup() {}
+  unSelect($cell) {
+    if (!$cell) {
+      return
+    }
+
+    this.selectedGroup.delete($cell)
+    $cell.classList.remove(Table.ClassList.SELECTED)
+
+    if (this.selected === $cell) {
+      this.selected = null
+    }
+  }
+
+  selectGroup($cells) {
+    $cells.forEach(this.select, this)
+  }
+
+  unselectGroup($cells = this.selectedGroup) {
+    $cells.forEach(this.unSelect, this)
+  }
+
+  addListeners() {
+    this.$dataCells.forEach(($dataCell) => {
+      $dataCell.addEventListener('focus', () => this.select($dataCell))
+    })
+  }
 }
